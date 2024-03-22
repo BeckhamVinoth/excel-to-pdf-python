7fbc0a7a2d53637bc0f77d125d304e9c373f0caf
BeckhamVinoth <vinoth.10decoders@gmail.com>
2024-03-22 18:13:02 +0530

    Invoice date added.

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ for path in file_paths:
     pdf.add_page()
     file_name = Path(path).stem
     invoice_num = file_name.split('-')[0]
+    invoice_date = file_name.split('-')[1]
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice num.{invoice_num}")
+    pdf.cell(w=50, h=8, txt=f"Invoice num.{invoice_num}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Date : {invoice_date}")
+
     pdf.output(f"pdfs/{file_name}.pdf")
 
